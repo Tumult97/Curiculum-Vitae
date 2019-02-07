d0066709337c6eaae47cdc4c4e6af24ec1299949
Unknown <tristanvdm87@gmail.com>
2019-02-07 19:57:06 +0200

    Setup commit.
    Added: 
    Node modules
    Added Routers for angular and header sub-route
    Upgraded to angular 7

---
+++ Server/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { NavibarComponent } from './navibar/navibar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
